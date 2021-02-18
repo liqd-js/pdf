ed2c4db8533b07ddde498ea49a71ffe7d564a04e
Tomáš Korenko <radixxko@gmail.com>
2021-02-18 12:57:30 +0100

    initial commmit

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 'use strict';
 
+/** /
 const PDF = require('../lib/pdf');
 
 const Invoice = new PDF( '<table><td>{ $props.test }</td></table>' );
 
 console.log( Invoice.render({ test: 'jozo' }) );
+
+/**/
+
+const Style = require('../lib/style');
+
+let style = new Style({ fontWeight: 'bold', background: 'red' });
+
+console.log( style );
+console.log( style.inherit() );
+
+style.apply( 'border: 1px solid silver; font-size: 10px' );
