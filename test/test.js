8c3cecc0f3ae849aae0eca8364e3e83a64f1b055
Tomáš Korenko <radixxko@gmail.com>
2021-07-28 15:56:12 +0200

    text

--- test/test.js
+++ test/test.js
@@ -3,17 +3,82 @@
 /**/
 const PDF = require('../lib/pdf');
 //<page> </page>
-const Invoice = new PDF( require('fs').readFileSync( __dirname + '/pdf.html', 'utf8' ));
+const Invoice = new PDF( require('fs').readFileSync( __dirname + '/pdf2.html', 'utf8' ));
 
-Invoice.render(
+const invoice = 
 {
-    no: '200012341',
-    items: 
-    [
-        { quantity: 1, name: 'Prvy item', price: 3.34 },
-        { quantity: 5, name: 'Druhy item', price: 19.28 }
-    ]
-});
+    "type": "invoice",
+    "no": "12100000004",
+    "issued": 1613636626,
+    "due": 1613895826,
+    "reference": "21000003",
+    "price": 95.7,
+    "currency": "EUR",
+    "seller": {
+        "name": "LUCULLUS s.r.o.",
+        "address": "Pribinova 4",
+        "zip": "811 09",
+        "city": "Bratislava",
+        "country": "SK",
+        "crn": "50169971",
+        "tax": "2120204361",
+        "vat": "SK2120204361",
+        "shipping": {
+            "name": "LUCULLUS s.r.o.",
+            "address": "Pribinova 4",
+            "zip": "811 09",
+            "city": "Bratislava",
+            "country": "SK"
+        },
+        "banks": [{
+            "country": "SK",
+            "account": "4321454853/0200",
+            "iban": "SK3502000000004321454853",
+            "swift": "SUBASKBX"
+        }]
+    },
+    "customer": {
+        "name": "Robert Hozza",
+        "address": "Uherová 15",
+        "zip": "05801",
+        "city": "Poprad",
+        "country": "SK",
+        "shipping": {
+            "name": "Robert Hozza",
+            "address": "Uherová 15",
+            "zip": "05801",
+            "city": "Poprad",
+            "country": "SK"
+        }
+    },
+    "items": [{
+        "uid": "3493",
+        "name": "Umelý Ficus Retusa Bonsai 50 cm",    
+        "physical": true,
+        "quantity": 3,
+        "price": 7.9,
+        "discount": 0,
+        "vat": 0.2
+    }, {
+        "uid": "3893",
+        "name": "Umelá rastlina Marginata v kvetináči z prírodného materiálu",
+        "physical": true,
+        "quantity": 4,
+        "price": 9.6,
+        "discount": 0,
+        "vat": 0.2
+    }, {
+        "uid": "4899",
+        "name": "Umelá guľa z machu 16 cm",    
+        "physical": true,
+        "quantity": 3,
+        "price": 11.2,
+        "discount": 0,
+        "vat": 0.2
+    }],
+}
+
+Invoice.render( invoice );
 
 /**/
 
